ab13392d881df1f57ff89913c9d059400e8add4e
Viktor Gense <viktor.gense@hdnet.de>
2026-05-05 14:00:05 +0200

    chore: Add version to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,13 +19,13 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.0.13',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-12.9.99',
+            'typo3' => '11.0.0-13.9.99',
             'container' => '2.3.1-3.9.99',
         ],
         'conflicts' => [
         ],
         'suggests' => [
-            'news' => '9.0.0-10.9.9'
+            'news' => '9.0.0-14.9.9'
         ],
     ],
 ];
